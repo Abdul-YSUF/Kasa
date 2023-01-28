9f5c675bdda62522abcdeb64646b294aea4dd64d
IALTAIR <yferoze@gmail.com>
2023-01-28 01:03:05 +0100

    Ajout Header et Banner

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../style/Header.css";
+import logo from '../assets/LOGO.svg';
+
+const Header = () => {
+  return (
+    <header className="navigation">
+        <NavLink to="/"><img className="logo" src={logo} alt="Logo Kasa"/></NavLink>
+      <nav>
+        <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}>
+          Accueil
+        </NavLink>
+        <NavLink to="/about" className={(nav) => (nav.isActive ? "nav-active" : "nav-inactive")}>
+          A Propos
+        </NavLink>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/pages/Accueil.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+
+const Accueil = () => {
+    return (
+        <div>
+            <Header />
+            <Banner />
+        </div>
+    );
+};
+
+export default Accueil;
